b112536d5c8686ef2a722a4c4cb5052fbef09053
Jie-Mac <e25254@gmail.com>
2022-10-11 15:00:19 +0800

    用post傳資料

--- index.js
+++ index.js
@@ -7,33 +7,42 @@ const app = express()
 app.set("view engine", "ejs")
 
 // routes 路由器
-app.get("/a", (req, res) => {
-    // res.send(`<h2>你好</h2>`)
-    res.render("main", { name: "Jie" })
+app.get("/", (req, res) => {
+	// res.send(`<h2>你好</h2>`)
+	res.render("main", { name: "Jie" })
 })
 
 app.get("/sales-json", (req, res) => {
-    const sales = require(__dirname + '/data/sales.json')
-    console.log(sales);
-    res.render(`sales-json`,{sales})
+	const sales = require(__dirname + "/data/sales.json")
+	console.log(sales)
+	res.render(`sales-json`, { sales })
 })
 
-app.get('/json-test', (req, res) => {
-        // res.send({ name: '小新1', age: 30 });
-        res.json({ name: '小新2', age: 30 });
-    });
+app.get("/json-test", (req, res) => {
+	// res.send({ name: '小新1', age: 30 });
+	res.json({ name: "小新2", age: 30 })
+})
+
+app.get("/try-qs", (req, res) => {
+	res.json(req.query)
+})
+
+const urlencodedParser = express.urlencoded({ extended: false })
+app.post("/try-post", urlencodedParser, (req, res) => {
+	res.json(req.body)
+})
 
 app.use(express.static(__dirname + "/public"))
 app.use(express.static(__dirname + "/node_modules/bootstrap/dist"))
 
 app.use((req, res) => {
-    // res.type("text/plain")
-    res.status(404).render("404")
+	// res.type("text/plain")
+	res.status(404).render("404")
 })
 
 // node_modules/bootstrap/dist/css/bootstrap.css
 
 const port = process.env.SERVER_PORT || 3002
 app.listen(port, () => {
-    console.log(`server started,port:${port} `)
+	console.log(`server started,port:${port} `)
 })
