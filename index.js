8c1772b9cad57b3e64f2f2b88400535e2d556579
Jie-Mac <e25254@gmail.com>
2022-10-11 09:46:34 +0800

    建立靜態頁面

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
+require('dotenv').config();
+
 const express = require('express')
 
 const app = express()
 
+app.use(express.static(__dirname+'/public'));
+
+// routes 路由器
 app.get('/', (req, res) => {
     res.send(`<h2>你好</h2>`)
 })
-app.listen(3000,()=>{
-    console.log('server started');
+
+app.get('/abc', (req, res) => {
+    res.send(`<h2>abc</h2>`)
+})
+
+app.use((req, res) => {
+    res.type('text/plain');
+    res.status(404).send('找不到你要的頁面')
+})
+
+
+const port = process.env.SERVER_PORT || 3002
+app.listen(port,()=>{
+    console.log(`server started,port:${port} `);
 })
